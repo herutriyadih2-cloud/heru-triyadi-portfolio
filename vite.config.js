deca2558999466fcf2769a18beaa0373074f3bed
Heru Triyadi <herutriyadih2@gmail.com>
2026-07-04 02:31:38 +0700

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
-  base: '/heru-triyadi-portfolio/', // <--- Tambahkan baris ini
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/heru-triyadi-portfolio/',
+  plugins: [vue()]
 })
